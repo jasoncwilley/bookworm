ba444a24b448d21b8846784f86c333820a3270fd
Matt Smith <msmith@apaxsoftware.com>
2016-09-20 15:43:10 -0400

    Setting up initial templates and views

--- bookcases/views.py
+++ bookcases/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Bookcase
+
+def bookcase_list(request):
+    bookcases = Bookcase.objects.prefetch_related('bookshelf_set').all()
+
+    context = {
+        "bookcases": bookcases,
+    }
+    return render(request, "bookcases/bookcase_list.html", context)
