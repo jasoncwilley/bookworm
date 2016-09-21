99217cac7291c27cc8fd32c2e1e0c73f22487bbb
Matt Smith <msmith@apaxsoftware.com>
2016-09-21 07:34:57 -0400

    Bookshelf detail with book thumbnail

--- bookcases/views.py
+++ bookcases/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
 from django.http import HttpResponse
 
-from .models import Bookcase
+from .models import Bookcase, Bookshelf
 
 
 def bookcase_list(request):
@@ -25,5 +25,15 @@ def bookcase_detail(request, id):
     return render(request, "bookcases/bookcase_detail.html", context)
 
 def bookshelf_detail(request, id):
+    query_set = Bookshelf.objects
+    query_set = query_set.select_related('bookcase')
+    query_set = query_set.annotate(book_count=Count('book'))
+    bookshelf = get_object_or_404(query_set, pk=id)
+    books = bookshelf.book_set.all()
+    
+    context = {
+        "bookshelf": bookshelf,
+        "books": books,
+    }
 
-    return HttpResponse("Bookshelf Detail")
+    return render(request, "bookcases/bookshelf_detail.html", context)
